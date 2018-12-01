fbaf0a4f7d8ee8cb07340d4c1d48f49258167c17
Ngareski <ngarejoseph@gmail.com>
2018-12-01 13:00:32 +0300

    [Finishes #162362231] Finished creating POST request

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,11 +3,13 @@ from flask import Blueprint
 
 from instance.config import app_config
 from redflags.views import RedFlag, RedFlags, UpdateRedComment, UpdateRedLocation
+from users.views import UsersReg
 
 version1 = Blueprint('api-v1', __name__, url_prefix= '/api/v1')
 api = Api(version1)
 
 api.add_resource(RedFlags, '/red-flags')
 api.add_resource(RedFlag, '/red-flags/<int:redflag_id>')
+api.add_resource(UsersReg, '/users')
 api.add_resource(UpdateRedComment, '/red-flags/<int:redflag_id>/comment')
 api.add_resource(UpdateRedLocation, '/red-flags/<int:redflag_id>/location')
--- app/api/v1/redflags/views.py
+++ app/api/v1/redflags/views.py
@@ -8,3 +8,38 @@ class RedFlags(Resource):
     """
     docstring for all redflags
     """
+    def __init__(self):
+        self.db = RedFlagModels()
+    
+    def get(self):
+        self.db.get_all()
+
+        return make_response(jsonify({
+            "status" : 200,
+            "data" : self.db.get_all()
+        }), 200)
+        
+        
+    def post(self):
+        
+        data = {
+            'createdOn' : datetime.datetime.utcnow(),
+            'createdBy' : request.json['createdBy'],
+            'type' : 'red-flags',
+            'location' : request.json.get('location', ""),
+            'status' : "draft",
+            'images' : request.json.get('images', ""),
+            'videos' : request.json.get('videos', ""),
+            'title' : request.json['title'],
+            'comment' : request.json.get('comment', "")
+        }
+        self.db.save(data)
+        
+        success_msg = {
+            "message" : "Created red-flag record"
+        }
+
+        return make_response(jsonify({
+            "status" : 201,
+            "data" : success_msg
+        }), 201)
