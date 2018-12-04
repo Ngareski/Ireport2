0f267d16852ddfd6d84443f36e9cb764ea878f5b
Ngareski <ngarejoseph@gmail.com>
2018-12-04 07:48:38 +0300

    i report

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 
 from instance.config import app_config
 from app.api.v1.redflags.views import RedFlag, RedFlags, UpdateRedComment, UpdateRedLocation
-from app.api.v1.users.views import UsersReg, UserGet, UserEdit
+from app.api.v1.users.views import UsersReg, UserLogin
 
 version1 = Blueprint('api-v1', __name__, url_prefix= '/api/v1')
 api = Api(version1)
@@ -13,5 +13,4 @@ api.add_resource(RedFlag, '/red-flags/<int:redflag_id>')
 api.add_resource(UpdateRedComment, '/red-flags/<int:redflag_id>/comment')
 api.add_resource(UpdateRedLocation, '/red-flags/<int:redflag_id>/location')
 api.add_resource(UsersReg, '/users')
-api.add_resource(UserGet, '/users/<int:user_id>')
-api.add_resource(UserEdit, '/users/<int:user_id>/number')
+api.add_resource(UserLogin, '/users/login')
