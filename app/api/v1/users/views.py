6e42ea06c01b6eb716ecf1ce329e2bcc8d65ca30
Ngareski <ngarejoseph@gmail.com>
2018-12-04 08:09:37 +0300

    [finishes #162405108] finished developing user signup endpoint

---
+++ app/api/v1/users/views.py
@@ -0,0 +1,41 @@
+from flask_restful import Resource
+from flask import jsonify, make_response, request, abort, Blueprint
+from .models import UserModels
+import datetime
+
+class UsersReg(Resource):
+    """
+    docstring for get all userss
+    """
+    def __init__(self):
+        self.db = UserModels()
+
+    def get(self):
+        self.db.get_all()
+        return make_response(jsonify({
+            "status" : 200,
+            "data" : self.db.get_all()
+        }), 200)
+
+    def post(self):
+        """
+        dosctring to create a users record
+        """
+        data = {
+
+            'firstname': request.json.get('firstname', ""),
+            'lastname' : request.json.get('lastname', ""),
+            'othernames' :request.json.get('othernames', ""),
+            'email' : request.json.get('email', ""),
+            'phoneNumber' : request.json.get('phoneNumber', ""),
+            'username' : request.json.get('username', ""),
+            'registered' : datetime.datetime.utcnow(),
+            'password' : request.json.get('password', ""),
+            'confirmpassword' : request.json.get('password',"")
+        }
+        self.db.save(data)
+
+        return make_response(jsonify({
+            "status" : 201,
+            "data" : "Succesfuly created account"
+        }), 201)
